4773066d0b99093c4e36ad82bb106084018d6c04
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2015-02-23 07:39:50 +0000

    CLASSES-1453 Show a message to instructors explaining the "Site Groups" feature.

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/site/NYUSiteInfoRename.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/site/NYUSiteInfoRename.java
@@ -11,27 +11,28 @@ import org.sakaiproject.site.cover.SiteService;
 
 public class NYUSiteInfoRename {
     public void applySettings(PortalRenderContext rcontext, Site site, String userId) {
+        boolean isInstructor = false;
+
         if (SecurityService.unlock(SiteService.SECURE_UPDATE_SITE, site.getReference())) {
             // NYU: Should we show siteinfo as "settings"?
+            isInstructor = true;
             rcontext.put("showSiteInfoAsSettings", "true");
-
-            // Actually, ignored in this case.
-            rcontext.put("showJoinableGroups", "false");
         } else {
             rcontext.put("showSiteInfoAsSettings", "false");
-            rcontext.put("showJoinableGroups", "false");
+        }
 
-            Collection<Group> userGroups = site.getGroupsWithMember(userId);
-            if (userGroups != null && !userGroups.isEmpty()) {
-                // If the user is already in a group, show the tool
-                rcontext.put("showJoinableGroups", "true");
-            } else {
-                // Or if there are joinable groups they may want to join, show it.
-                for (Group g : site.getGroups()) {
-                    if (g.getProperties().getProperty(Group.GROUP_PROP_JOINABLE_SET) != null) {
-                        rcontext.put("showJoinableGroups", "true");
-                        break;
-                    }
+        rcontext.put("showJoinableGroups", "false");
+
+        Collection<Group> userGroups = site.getGroupsWithMember(userId);
+        if (!isInstructor && userGroups != null && !userGroups.isEmpty()) {
+            // If the user is already in a group, show the tool
+            rcontext.put("showJoinableGroups", "true");
+        } else {
+            // Or if there are joinable groups they may want to join, show it.
+            for (Group g : site.getGroups()) {
+                if (g.getProperties().getProperty(Group.GROUP_PROP_JOINABLE_SET) != null) {
+                    rcontext.put("showJoinableGroups", "true");
+                    break;
                 }
             }
         }
