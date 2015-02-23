044a01079fa778424903bd4648ef43207a0430b8
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2015-02-23 06:56:54 +0000

    CLASSES-1340 PDA template and corresponding CSS
    Bring across the 2.8.1 defaultskin/pda.vm template and merge with the neoskin/pda.vm.
    Also copy across the 2.8.1 new-york-u/pda_nyu.css and use this stylesheet for the 10.1 neo-new-york-u/pda.css stylesheet.

---
+++ portal/portal-charon/charon/src/webapp/scripts/pda.js
@@ -0,0 +1,89 @@
+// NYU Script for PDA Specific Javascript
+$(function() {
+
+  var PORTAL_BODY_SELECTOR = "body.portalBodyPDA";
+
+  var displaySelectInlineHelp = function() {
+    // Only show help tip for 'multiple' selects
+    // as single selects look like dropdown lists
+    $("select[multiple]", PORTAL_BODY_SELECTOR).after("<small class='select-inline-help'>Tap to select option(s)</small>");
+  };
+
+	var displayWelcomePopup = function() {
+		if ($("#pdaWelcomeDialog").length > 0) {
+			var _onScroll = function() {
+				$('#qtip-blanket').css("top", $(document).scrollTop());
+			};
+
+			var _onResize = function(event) {
+				$("#pdaWelcomeDialog").qtip('api').updateWidth($(window).width() - 20);
+				$("#pdaWelcomeDialog").qtip('api').updatePosition(event, false);
+				$('#qtip-blanket').css("height", $(document).height());
+			};
+
+			$("#pdaWelcomeDialog").qtip({
+				content: {
+					title: {
+						text: "Welcome to NYU Classes Mobile",
+						button: '<a href="#"><img src="/library/image/silk/cancel.png"/></a>'
+					},
+					text: $("#pdaWelcomeDialogContent").html()
+				},
+				position: {
+					target: $(document.body), // Position it via the document body...
+					corner: 'center' // ...at the center of the viewport
+				},
+				show: {
+					ready: true, // Show it when ready
+					solo: true // And hide all other tooltips
+				},
+				hide: false,
+				style: {
+					width: {
+						max: $(document).width() - 20,
+					},
+					border: {
+						width: 9,
+						radius: 9,
+						color: '#666666'
+					},
+					padding: '14px',
+					name: 'light'
+				},
+				api: {
+					beforeShow: function() {
+						$('<div id="qtip-blanket">')
+								.css({
+									 position: 'absolute',
+									 top: $(document).scrollTop(),
+									 left: 0,
+									 height: $(document).height(),
+									 width: '100%',
+									 opacity: 0.7,
+									 backgroundColor: 'black',
+									 zIndex: 5000
+								}).appendTo(document.body).fadeIn();
+						$(window).bind("scroll", _onScroll);
+						$(window).bind("resize", _onResize);
+					},
+					onHide: function() {
+						$('#qtip-blanket').fadeOut(function() {
+							$('#qtip-blanket').remove();
+						});
+						$("#previewInfoDialog").qtip("destroy");
+						$(window).unbind("scroll", _onScroll);
+						$(window).unbind("resize", _onResize);
+					},
+				}
+			});
+
+      $("#pdaPopupContinue").click(function() {
+        $("#pdaWelcomeDialog").qtip("hide");
+      });
+		}
+	};
+
+  // Things to run on document ready
+  displaySelectInlineHelp();
+  displayWelcomePopup();
+});
