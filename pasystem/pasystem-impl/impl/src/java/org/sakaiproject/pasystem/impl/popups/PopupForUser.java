fc6726bfaddbab7fcd722c7ddbf789ed600e6211
Mark Triggs <mark@dishevelled.net>
2015-05-20 19:07:37 +1000

    Knock out PopupImpl

--- pasystem/pasystem-impl/impl/src/java/org/sakaiproject/pasystem/impl/popups/PopupForUser.java
+++ pasystem/pasystem-impl/impl/src/java/org/sakaiproject/pasystem/impl/popups/PopupForUser.java
@@ -33,7 +33,7 @@ public class PopupForUser {
     public Popup getPopup() {
         if (user == null) {
             // No user.
-            return PopupImpl.createNullPopup();
+            return Popup.createNullPopup();
         }
 
         String sql = ("SELECT popup.uuid, popup.descriptor, popup.start_time, popup.end_time, content.template_content " +
@@ -85,7 +85,7 @@ public class PopupForUser {
                                             String templateContent = contentClob.getSubString(1, (int)contentClob.length());
 
                                             // Got one!
-                                            return PopupImpl.createPopup(result.getString(1),
+                                            return Popup.createPopup(result.getString(1),
                                                     result.getString(2),
                                                     result.getLong(3),
                                                     result.getLong(4),
@@ -93,13 +93,13 @@ public class PopupForUser {
                                         }
 
                                         // Otherwise, no suitable popup was found
-                                        return PopupImpl.createNullPopup();
+                                        return Popup.createNullPopup();
                                     }
                                 }
                             });
         } catch (Exception e) {
             LOG.error("Error determining active popup", e);
-            return PopupImpl.createNullPopup();
+            return Popup.createNullPopup();
         }
     }
 
