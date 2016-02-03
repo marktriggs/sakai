e2f4bb6c378a96cd3036ada0be9be7e645c6e961
pg1025@nyu.edu <pg1025@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-02-03 00:14:28 +0000

    CLASSES-2030 ensure the sorted section ids are used to populate the editClass listing

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -4333,6 +4333,10 @@ public class SiteAction extends PagedResourceActionII {
 		List providerCourseList = SiteParticipantHelper.getProviderCourseList((String) state.getAttribute(STATE_SITE_INSTANCE_ID));
 		if (providerCourseList != null && providerCourseList.size() > 0) {
 			rv = true;
+
+			// CLASSES-455 CLASSES-2030 NYU Change - sort course list by id
+			Collections.sort(providerCourseList);
+
 			state.setAttribute(SITE_PROVIDER_COURSE_LIST, providerCourseList);
 			
 			Hashtable<String, String> sectionTitles = new Hashtable<String, String>();
@@ -4354,10 +4358,6 @@ public class SiteAction extends PagedResourceActionII {
 				}
 			}
 			context.put("providerCourseTitles", sectionTitles);
-
-                        // CLASSES-455 NYU Change - sort course list by id
-                        Collections.sort(providerCourseList);
-
 			context.put("providerCourseList", providerCourseList);
 		}
 
